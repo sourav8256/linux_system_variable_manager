df6dfca4d5d68be92d5e92a40940e0bf1e1a384e
sourav mandal <souravmandalm@gmail.com>
2019-06-11 23:45:12 +0530

    fixed icon in install.sh

--- install.sh
+++ install.sh
@@ -5,5 +5,5 @@ chmod +x /usr/local/bin/pathmanager.jar;
 cp pathmanager.desktop ~/.local/share/applications;
 chmod +x ~/.local/share/applications/pathmanager.desktop;
 mkdir /usr/share/pathmanager
-cp icon.webp /usr/share/pathmanager
+cp icon.png /usr/share/pathmanager
 echo "probably installed IDK LOL!";
